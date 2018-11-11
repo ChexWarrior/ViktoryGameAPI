a51b8ea198b555669e9b54f7f3f5804fdd1c8eb3
Andrew Lehman <aplehm@gmail.com>
2018-11-11 12:41:43 -0500

    Rename start game test

---
+++ tests/startGame.test.js
@@ -0,0 +1,23 @@
+const api = require('../api.js');
+
+(async () => {
+  await api.startGame({
+    title: 'My test game',
+    numPlayers: 2,
+    players: [
+      {
+        name: 'Andy',
+        alias: 'Ceezy The Great',
+        order: 2,
+        email: 'aplehm@gmail.com',
+      },
+      {
+        name: 'Joan',
+        order: 1,
+        email: 'aplehm@gmail.com',
+      },
+    ],
+  });
+})();
+
+
