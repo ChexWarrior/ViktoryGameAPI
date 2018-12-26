c25c103ffab174fd016c1f4435293b54741bbffa
Andrew Lehman <aplehm@gmail.com>
2018-12-26 09:36:15 -0500

    Verify that test server is up and running before tests

--- tests/startGame.test.js
+++ tests/startGame.test.js
@@ -1,23 +1,36 @@
-const api = require('../app/api/api.js');
+const puppeteer = require('puppeteer');
+// const api = require('../app/api/api.js');
 
-test('Verify that 2 player game is created', async () => {
-  const response = await api.startGame({
-    title: 'Test Game 2',
-    players: [
-      {
-        name: 'Andy',
-        alias: 'Ceezy The Great',
-        order: 2,
-        email: 'viktory2tester@yahoo.com',
-      },
-      {
-        name: 'Joan',
-        order: 1,
-        email: 'al95788n@pace.edu',
-      },
-    ],
-  });
+test('Verify that test page is up!', async () => {
+  const browser = await puppeteer.launch();
+  try {
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8080/startGame.html');
 
-  expect(response.success).toBeTruthy();
-  expect(response.url).toBeTruthy();
+    expect(await page.title()).toBe('startGame API Test Page');
+  } finally {
+    await browser.close();
+  }
 });
+
+// test('Verify that 2 player game is created', async () => {
+//   const response = await api.startGame({
+//     title: 'Test Game 2',
+//     players: [
+//       {
+//         name: 'Andy',
+//         alias: 'Ceezy The Great',
+//         order: 2,
+//         email: 'viktory2tester@yahoo.com',
+//       },
+//       {
+//         name: 'Joan',
+//         order: 1,
+//         email: 'al95788n@pace.edu',
+//       },
+//     ],
+//   });
+
+//   expect(response.success).toBeTruthy();
+//   expect(response.url).toBeTruthy();
+// });
