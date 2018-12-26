6b9e779fa2499066cb6d0d468394cb07835bdb43
Andrew Lehman <aplehm@gmail.com>
2018-12-26 09:34:52 -0500

    Correct location of test html files

--- tests/setup/jest-setup.js
+++ tests/setup/jest-setup.js
@@ -3,5 +3,5 @@ const childProcess = require('child_process');
 module.exports = async () => {
   // eslint-disable-next-line
   console.log('\nStarting test server...');
-  childProcess.exec('docker container run --rm -dp 8080:80 -v $PWD/tests/html:/usr/share/nginx/html:ro --name viktory_test_server nginx');
+  childProcess.exec('docker container run --rm -dp 8080:80 -v $PWD/tests/setup/server:/usr/share/nginx/html:ro --name viktory_test_server nginx');
 };
